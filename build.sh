dc3188b039ff36be6dab6bf39e7c3d4ecbcb72b7
Filip Konieczny <z1172480@student.tcs.uj.edu.pl>
2025-08-23 20:59:21 +0200

    p3: Fix CI 2

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ DIST_DIR=dist
 
 # Haskell
 echo "Building Haskell..."
-ghc -package-db network-3.2.8.0/dist-newstyle/packagedb/ghc-9.4.7 -package network -isrc -o ${DIST_DIR}/main -odir ${BUILD_DIR} src/Main.hs 
+ghc -v -package-db network-3.2.8.0/dist-newstyle/packagedb/ghc-9.4.7 -package network -isrc -o ${DIST_DIR}/main -odir ${BUILD_DIR} src/Main.hs 
 
 # C
 # Compile if we have `rpc.h` header file, otherwise use provided binary (e.g. on `student`)
 echo "Building C..."
 if [ -f /usr/include/rpc/rpc.h ] ; then
-    make -C rpcc -f Makefile.kvstore kvstore_server -B
+    make --no-silent -C rpcc -f Makefile.kvstore kvstore_server -B
 else
     echo "Skipping C compilation, using precompiled binary"
 fi
